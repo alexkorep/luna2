bc3455dea768165b21f6b2450ef6cf92ff26c402
Alexey Korepanov <alexkorep@gmail.com>
2023-12-28 08:37:58 +0100

    Updates

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1703109553|3704604847";
+const CACHE_VERSION = "1703749031|96730969192";
 const CACHE_PREFIX = "Luna2-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
